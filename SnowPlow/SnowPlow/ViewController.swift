e239abe3ed65ab10966947f6bc8cd4adf1ab8431
Michael Baraty <mbaraty20@stjohnsprep.org>
2018-12-10 14:28:29 -0500

    xCode initial commit

---
+++ SnowPlow/SnowPlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowPlow
+//
+//  Created by Michael Baraty on 12/10/18.
+//  Copyright © 2018 Baraty Hannibal Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
