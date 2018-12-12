addc7d6a60973efc142165b19eeb1662492e94b3
Michael Baraty <mbaraty20@stjohnsprep.org>
2018-12-12 10:43:13 -0500

    PARSE IS AMAZING NOW; IT WORKS

---
+++ SnowPlow/SnowPlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowPlow
+//
+//  Created by Michael Baraty on 12/12/18.
+//  Copyright © 2018 Baraty Hannibal Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
